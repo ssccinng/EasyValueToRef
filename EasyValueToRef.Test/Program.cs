8fa49c48b70140e166b8b85d66f42532475ae31b
scixing <whs11998@163.com>
2023-04-24 09:55:50 +0800

    添加项目文件。

---
+++ EasyValueToRef.Test/Program.cs
@@ -0,0 +1,64 @@
+﻿// See https://aka.ms/new-console-template for more information
+using EasyValueToRef;
+
+Ref<int> a = 2;
+Ref<int> b = 1;
+
+Ref<bool> v = true;
+var cc = a;
+if (v)
+{
+    Console.WriteLine("v is true");
+}
+
+var bb = v;
+
+bb.SetValue(false);
+
+Console.WriteLine(v);
+if (v)
+{
+    Console.WriteLine("v is true");
+}
+
+Console.WriteLine(a + 32);
+a.SetValue(a - 32);
+if (a * a == 1)
+{
+    Console.WriteLine("a big than 1");
+}
+else if (a == 1)
+{
+    Console.WriteLine("a equal with 1");
+}
+else
+{
+    Console.WriteLine("a small than 1");
+}
+
+MyClass my = new MyClass();
+
+MyClass2 myClass2 = new MyClass2(my.bool1);
+Console.WriteLine(myClass2.bool1);
+my.bool1.SetValue(true);
+Console.WriteLine(myClass2.bool1);
+
+class MyClass
+{
+    public Ref<bool> bool1 = false;
+    public Ref<int> Num = 1;
+
+   
+}
+
+class MyClass2
+{
+    public Ref<bool> bool1 = false;
+    public Ref<int> Num = 1;
+
+    public MyClass2(Ref<bool> b)
+    {
+        bool1 = b;
+    }
+}
+
---
+++ EasyValueToRef/EasyValueToRef.cs
@@ -0,0 +1,88 @@
+﻿using System.Numerics;
+
+namespace EasyValueToRef
+{
+    public class Ref<T> where T : struct
+    {
+        private T _value;
+        // 重载运算符使得他能和t直接+-
+        //public static T operator +(Ref<T> r, T t)
+        //{
+        //    r.Value = t;
+        //    return r.Value;
+        //}
+        public static implicit operator Ref<T>(T v)
+        {
+            return new Ref<T> { _value = v };
+        }
+
+        // 重载使得能够解构为T
+        public static implicit operator T(Ref<T> r)
+        {
+            return r._value;
+        }
+
+        // 重载toString
+        public override string ToString()
+        {
+            return _value.ToString() ?? string.Empty;
+        }
+
+        public static bool operator ==(Ref<T> r, Ref<T> t)
+        {
+            return r._value.Equals(t._value);
+        }
+
+        public static bool operator !=(Ref<T> r, Ref<T> t)
+        {
+            return !r._value.Equals(t._value);
+        }
+
+        public void SetValue(T value) { _value = value; }
+
+        public override bool Equals(object obj)
+        {
+            return this == obj;
+        }
+
+        public override int GetHashCode()
+        {
+            return _value.GetHashCode();
+        }
+    }
+}
+//    public class RefNum<T> : Ref<T> where T : struct, INumber<T>
+//    {
+//        //public static T operator +(RefNum<T> r, T t)
+//        //{
+//        //    return r.Value + t;
+//        //}
+
+//        public static implicit operator RefNum<T>(T v)
+//        {
+//            return new RefNum<T> { Value = v };
+//        }
+
+//        // 生成方法使之能够比较大小
+//        public static bool operator >(RefNum<T> r, T t)
+//        {
+//            return r.Value.CompareTo(t) > 0;
+//        }
+
+//        public static bool operator <(RefNum<T> r, T t)
+//        {
+//            return r.Value.CompareTo(t) < 0;
+//        }
+
+//        // 生成方法使之能够比较是否相等
+//        public static bool operator ==(RefNum<T> r, T t)
+//        {
+//            return r.Value.CompareTo(t) == 0;
+//        }
+
+//        public static bool operator !=(RefNum<T> r, T t)
+//        {
+//            return r.Value.CompareTo(t) != 0;
+//        }
+//    }
+//}
